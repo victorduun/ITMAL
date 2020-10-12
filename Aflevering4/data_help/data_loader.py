6fb1305af00f6e7b7efde0ef029812e2052f60f7
victorduun <43340964+victorduun@users.noreply.github.com>
2020-10-12 15:24:41 +0200

    Lektion 5 øvelse 2 (Egen data)

--- Aflevering4/data_help/data_loader.py
+++ Aflevering4/data_help/data_loader.py
@@ -1,16 +1,31 @@
-import pandas as pd
+import json
 import numpy as np
+from dataclasses import dataclass
 
 path = r'C:\Users\victo\Aarhus Universitet\Test - Dokumenter\Elektronik\Noter Elektronik\Elektronik 6. semester\ITMAL\ITMAL - Exercises\Aflevering4\ship_data\shipsnet.json'
 
 
-def load_ship_data_numpy_array():
-    df = pd.read_json(path)
-    data = np.array(df)
-    return data
+@dataclass
+class ShipsData:
+    images: np.array
+    labels: np.array
+    locations: np.array
+    scene_ids: np.array
+
+
+def load_ship_data() -> ShipsData:
+    with open(path) as file:
+        dataset = json.load(file)
+        # dataset.keys() # - dict_keys(['data', 'labels', 'locations', 'scene_ids'])
+        images = np.array(dataset["data"]).astype("uint8")
+        labels = np.array(dataset["labels"]).astype("uint8")
+        locations = np.array(dataset["locations"]).astype("float")
+        scene_ids = np.array(dataset["scene_ids"]).astype("str")
+        img_length = 80
+        images = images.reshape(-1, 3, img_length, img_length).transpose([0, 2, 3, 1])
+    ships = ShipsData(images, labels, locations, scene_ids)
+    return ships
 
 
 def get_path():
     return path
-
-
